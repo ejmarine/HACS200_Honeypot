be4610d20a10dfe19faeed7968144999350e52f3
Teeddy-LB <83368661+ejmarine@users.noreply.github.com>
2025-10-02 12:52:32 -0400

    Update main.sh

--- recycling/main.sh
+++ recycling/main.sh
@@ -29,12 +29,16 @@ while true; do
 
   # Watch MITM log from the end only
   tail -F "$LOGFILEPATH" | while read -r line; do
-    if echo "$line" | grep -q "Opened shell for attacker"; then
+    #if echo "$line" | grep -q "Opened shell for attacker"; then
     # ADD INACTIVE TIMEOUT
-      if timeout 600s grep -q "Attacker closed connection"; then
-        break
-      fi
-    fi
+      #if timeout 600s grep -q "Attacker closed connection"; then
+      #  break
+      #fi
+      
+    #fi
+    echo "[*] Testing the recycle.sh script after 1 minute..."
+    sleep 60
+    break
   done
 
   ./recycle.sh "$CONTAINER" "$EXTERNAL_IP" "$MITM_PORT"

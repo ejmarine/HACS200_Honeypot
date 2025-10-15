314f94a4c12be083253ec2a09ca9bd088e94fbd9
Teeddy-LB <83368661+ejmarine@users.noreply.github.com>
2025-10-15 15:48:35 -0400

    Update main.sh

--- recycling/main.sh
+++ recycling/main.sh
@@ -68,7 +68,7 @@ while true; do
 
   screen -S $CONTAINER -X quit 2>/dev/null
 
-  screen -dmS $CONTAINER node /root/honeypots/MITM/mitm/index.js $CONTAINER >> "../logs/$CONTAINER.out" 2>&1
+  screen -dmS $CONTAINER node /root/honeypots/MITM/mitm/index.js $CONTAINER >> "../logs/$CONTAINER.out" >/dev/null 2>&1
 
   echo "[*] MITM server started"
 

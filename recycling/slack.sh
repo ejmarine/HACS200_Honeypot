6950d403dc356e031f7589eee610b260e62b07d9
Teeddy-LB <83368661+ejmarine@users.noreply.github.com>
2025-10-16 11:59:03 -0400

    Update slack.sh

--- recycling/slack.sh
+++ recycling/slack.sh
@@ -7,13 +7,15 @@ fi
 
 message=$1
 
+echo "[*] Sending message to Slack: $message"
+
 token=$(cat token.env)
 
 channel="C09LR132PA7"
 
 curl -X POST \
     -H 'Content-type: application/json; charset=utf-8' \
-    --data '{ "channel": "'$channel'", "icon_emoji": ":bot:", "text": "'$message'" }' \
+    --data '{ "channel": "'$channel'", "icon_emoji": ":bot:", "text": '$message' }' \
     $token
 
 echo "Message sent to Slack"

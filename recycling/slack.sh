8faab8d4648e50ce56364db3e3320121333ad996
Teeddy-LB <83368661+ejmarine@users.noreply.github.com>
2025-10-16 11:23:19 -0400

    Added slack

---
+++ recycling/slack.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+token = $(cat token.env)
+
+channel = "C09LR132PA7"
+
+curl -X POST -H "Authorization: Bearer $token" \
+     -H "Content-Type: application/json" \
+     --data '{"channel":"$channel","text":"Hello from curl!"}' \
+     https://slack.com/api/chat.postMessage
+
+echo "Message sent to Slack"

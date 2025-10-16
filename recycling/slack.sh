74056897d63382d6e63628b0d1da3553a4c08833
ACES <edwardtjmarine@gmail.com>
2025-10-16 11:41:56 -0400

    updated slack

--- recycling/slack.sh
+++ recycling/slack.sh
@@ -4,9 +4,9 @@ token=$(cat token.env)
 
 channel="C09LR132PA7"
 
-curl -X POST -H "Authorization: Bearer $token" \
-     -H "Content-Type: application/json" \
-     --data '{"channel":"'$channel'","text":"Hello from curl!"}' \
-     https://slack.com/api/chat.postMessage
+curl -X POST \
+    -H 'Content-type: application/json; charset=utf-8' \
+    --data '{ "channel": "'$channel'", "icon_emoji": ":bot:", "text": "Foo" }' \
+    $token
 
 echo "Message sent to Slack"

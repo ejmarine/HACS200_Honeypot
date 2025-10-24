b21d68824a6feb2c707ce59ae04d9d8b0218c246
Teeddy-LB <83368661+ejmarine@users.noreply.github.com>
2025-10-24 18:32:01 -0400

    Update main.sh

--- recycling/main.sh
+++ recycling/main.sh
@@ -110,14 +110,14 @@ while true; do
           ATTACKER_IP=$(echo "$line" | cut -d':' -f4 | cut -d' ' -f2)
           echo "[*] Attacker IP: $ATTACKER_IP"
           # Only allow SSH connections from the attacker's IP to the container's IP
-          sudo /sbin/iptables -A FORWARD -i lxcbr0 -s "$ATTACKER_IP" -d "$INTERNAL_IP" -p tcp --dport 22 -j ACCEPT
-          sudo /sbin/iptables -A FORWARD -i lxcbr0 -d "$INTERNAL_IP" -p tcp --dport 22 -j DROP
-      
+          sudo /sbin/iptables -I FORWARD -d "$INTERNAL_IP" -p tcp --dport $MITM_PORT -j DROP
+          sudo /sbin/iptables -I FORWARD -s "$ATTACKER_IP" -d "$INTERNAL_IP" -p tcp --dport $MITM_PORT -j ACCEPT
+          
       elif echo "$line" | grep -q "Attacker closed connection"; then
             # Clear any existing rules for this attacker/container combo if disconnected
           # Undo exactly the previous iptables command
-          sudo /sbin/iptables -D FORWARD -i lxcbr0 -s "$ATTACKER_IP" -d "$INTERNAL_IP" -p tcp --dport 22 -j ACCEPT
-          sudo /sbin/iptables -D FORWARD -i lxcbr0 -d "$INTERNAL_IP" -p tcp --dport 22 -j DROP
+          sudo /sbin/iptables -D FORWARD -s "$ATTACKER_IP" -d "$INTERNAL_IP" -p tcp --dport $MITM_PORT -j ACCEPT
+          sudo /sbin/iptables -D FORWARD -d "$INTERNAL_IP" -p tcp --dport $MITM_PORT -j DROP
 
       elif echo "$line" | grep -q "Adding the following credentials:"; then
 
@@ -233,8 +233,8 @@ while true; do
       AVG_TIME="N/A"
   fi
   
-  sudo /sbin/iptables -D FORWARD -i lxcbr0 -s "$ATTACKER_IP" -d "$INTERNAL_IP" -p tcp --dport 22 -j ACCEPT
-  sudo /sbin/iptables -D FORWARD -i lxcbr0 -d "$INTERNAL_IP" -p tcp --dport 22 -j DROP
+  sudo /sbin/iptables -D FORWARD -s "$ATTACKER_IP" -d "$INTERNAL_IP" -p tcp --dport $MITM_PORT -j ACCEPT
+  sudo /sbin/iptables -D FORWARD -d "$INTERNAL_IP" -p tcp --dport $MITM_PORT -j DROP
   # Send Slack notifications
   /home/aces/HACS200_Honeypot/recycling/helpers/slack.sh "$CONTAINER" "$CONTAINER - Attacker $ATTACKER_IP ran: $COMMANDS" &
   
